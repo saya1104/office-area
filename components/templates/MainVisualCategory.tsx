d074e772c5f89c6799baa9f80d0fc0ac81af1232
中井礼 <nakaiaya@nakaiayanoMacBook-Pro.local>
2023-04-19 16:45:02 +0900

    ✨ メインビジュアルを実装

--- components/templates/MainVisualCategory.tsx
+++ components/templates/MainVisualCategory.tsx
@@ -1,22 +1,79 @@
 import Image from 'next/image';
 import styled from 'styled-components';
 import { GradationStyle } from '../../styles/Styled';
+import { sp, tablet } from '../../styles/breakPoint';
 
 export const MainStyle = styled.div`
   display: flex;
   justify-content: space-between;
+  position: relative;
+  ${sp`
+  display: block;
+  overflow: hidden;`}
 `;
 
-export const TextStyle = styled.div``;
+export const TextStyle = styled.div`
+  position: absolute;
+  top: 254px;
+  left: 58px;
+  z-index: 1;
+  ${sp`
+  position: relative;
+  top: 0;
+  left: 0;`}
+  h2 {
+    ${GradationStyle}
+    white-space: nowrap;
+    ${tablet`
+    // font-size: 6vw;
+    font-size: 60px;
+    `}
+    ${sp`
+    font-size: 40px;`}
+  }
+  p {
+    font-size: 30px;
+    white-space: nowrap;
+    ${sp`
+    font-size: 20px;`}
+  }
+`;
+
+export const ImageStyle = styled.div`
+  z-index: 0;
+  margin: 0 0 0 auto;
+  width: 1002px;
+  height: 938px;
+  ${sp`
+  width: 120%;
+  height: auto;
+  margin-left: -10%;`}
+`;
 
 const MainVisualCategory = () => {
   return (
     <MainStyle>
+      <ImageStyle>
+        <Image
+          src="/assets/MainVisual.png"
+          alt="MainVisual"
+          width={1002}
+          height={938}
+          sizes="100vw"
+          style={{
+            width: '100%',
+            height: 'auto',
+          }}
+        />
+      </ImageStyle>
       <TextStyle>
-        <GradationStyle>京都発の技術でサービスを革新する</GradationStyle>
+        <h2>
+          京都発の技術で
+          <br />
+          サービスを革新する
+        </h2>
         <p>AIFUL ENGINEER RECRUITMENT</p>
       </TextStyle>
-      <Image src="/assets/MainVisual.png" alt="Main" width={938} height={1002} />
     </MainStyle>
   );
 };
--- styles/globalstyles.tsx
+++ styles/globalstyles.tsx
@@ -11,6 +11,14 @@ body {
 	font-size: 18px;
 	font-family: 'Inter';
 	width: 100%;
+}
+h1 {
+	margin: 0;
+}
+h2 {
+	font-family: 'YuGothic';
+	font-size: 87px;
+	margin: 0;
 }`;
 
 export default GlobalStyle;
