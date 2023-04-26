b68bc1eaedda09797f1db973d1cdfea86efea639
中井礼 <nakaiaya@nakaiayanoMacBook-Pro.local>
2023-04-26 13:29:10 +0900

    ✨ swiperの実装、styled-componentsのエラー解消

--- components/templates/InterviewCategory.tsx
+++ components/templates/InterviewCategory.tsx
@@ -1,7 +1,12 @@
 import styled from 'styled-components';
+import { Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { GradationStyle } from '../../styles/Styled';
 import { sp } from '../../styles/breakPoint';
 import InterviewMember from '../elements/InterviewMember';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 const InterviewStyle = styled.div`
   margin: 0 auto;
@@ -24,14 +29,14 @@ const InterviewStyle = styled.div`
   }
 `;
 
-const MembersListStyle = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-top: 100px;
-  margin-right: 8%;
-  margin-left: 8%;
-`;
+// const MembersListStyle = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   margin-top: 100px;
+//   margin-right: 8%;
+//   margin-left: 8%;
+// `;
 
 const InterviewCategory = () => {
   return (
@@ -42,12 +47,40 @@ const InterviewCategory = () => {
           メンバー紹介テキスト。メンバー紹介テキスト。メンバー紹介テキスト。メンバー紹介テキスト。メンバー紹介テキスト。
         </p>
       </InterviewStyle>
-      <MembersListStyle>
+      {/* <MembersListStyle>
         <InterviewMember />
         <InterviewMember />
         <InterviewMember />
         <InterviewMember />
-      </MembersListStyle>
+      </MembersListStyle> */}
+      {/* <SwiperMembersStyle> */}
+      <Swiper
+        // 一度に表示するスライドの数
+        slidesPerView={4}
+        // スライド間の余白
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <InterviewMember />
+        </SwiperSlide>
+        <SwiperSlide>
+          <InterviewMember />
+        </SwiperSlide>
+        <SwiperSlide>
+          <InterviewMember />
+        </SwiperSlide>
+        <SwiperSlide>
+          <InterviewMember />
+        </SwiperSlide>
+        <SwiperSlide>
+          <InterviewMember />
+        </SwiperSlide>
+      </Swiper>
     </>
   );
 };
