65dfa2df85cf21fdab34dc2e4e4eba888713fa7d
kaneko1117 <yuta_kaneko@pj100.biz>
2023-05-15 09:06:36 +0900

    ✨svgを動かす

--- components/templates/MainVisual.tsx
+++ components/templates/MainVisual.tsx
@@ -6,11 +6,10 @@ import { sp, tablet } from '../../styles/breakPoint';
 import { gsap } from 'gsap';
 
 const MainVisualStyle = styled.div`
-  display: flex;
-  justify-content: space-between;
   position: relative;
   ${sp`
   display: block;
+  height:150vw;
   overflow: hidden;`}
 `;
 
@@ -20,8 +19,7 @@ const TextStyle = styled.div`
   left: 58px;
   z-index: 1;
   ${sp`
-  position: relative;
-  top: 0;
+  top:110vw;
   left: 0;
   margin: 10px;
   `}
@@ -52,13 +50,20 @@ const ImageStyle = styled.div`
   width: 1002px;
   height: 938px;
   ${sp`
-  width: 120%;
-  height: auto;
-  margin-left: -10%;`}
+    width:100vw;
+    height:auto;
+  `}
 `;
 
 const PersonImageStyle = styled.div`
   position: absolute;
+  top: 5px;
+  left: 5px;
+  ${sp`
+  width: 120%;
+  height: auto;
+  margin-left: -10%
+`};
 `;
 
 const FrameImageStyle = styled.div`
@@ -66,6 +71,13 @@ const FrameImageStyle = styled.div`
   animation-name: Frame-Animation;
   animation-iteration-count;
   animation-duration: 1s;
+  width:1025px;
+  height:1030px;
+  ${sp`
+    width: 120%;
+    height: auto;
+    margin-left: -10%
+  `}
 `;
 
 const MainVisual = () => {
@@ -73,9 +85,9 @@ const MainVisual = () => {
   useEffect(() => {
     gsap.to(imageRef.current, {
       attr: {
-        d: 'M1002 0H0V1027H1002V0ZM114.278 566.749C164.635 493.856 251.325 368.371 259 313.5C274.095 205.58 390.707 100.863 533.5 52.0001C676.293 3.13691 812.5 95.4999 843.676 172.424C849.864 187.69 867.723 204.278 889.104 224.137C940.178 271.574 1011.34 337.672 991.5 449C965.398 595.442 920.756 646.972 813.043 771.305C804.591 781.061 795.75 791.266 786.5 802C708.181 892.879 445.079 917.588 337.372 908.426C229.666 899.264 -103.963 870.11 93.5 597C99.2021 588.571 106.282 578.323 114.278 566.749Z',
+        d: 'M1025 0H0V1030H1025V0ZM146.898 371.679C152.765 338.008 158.427 305.515 179.073 278.73C188.094 267.027 196.943 252.074 206.88 235.282C244.181 172.251 296.821 83.2998 431.5 42.9702C602.059 -8.10353 879.659 68.0511 920.593 114.109C921.509 115.139 922.422 116.164 923.332 117.185C963.064 161.787 995.872 198.617 963 360C929.376 525.078 844.584 635.103 732.015 731.322C619.446 827.541 444.491 883.076 315.841 873.5C187.191 863.924 -127.612 764.387 108.247 478.921C134.022 445.579 140.579 407.946 146.898 371.679Z',
       },
-      duration: 5,
+      duration: 10,
       repeat: -1,
       yoyo: true,
     });
@@ -97,24 +109,12 @@ const MainVisual = () => {
           />
         </PersonImageStyle>
         <FrameImageStyle>
-          {/* <Image
-            ref={imageRef}
-            src="/assets/MainVisualFream.svg"
-            alt="alful engineer recruitment"
-            width={1002}
-            height={1027}
-            sizes="100vw"
-            style={{
-              width: '100%',
-              height: 'auto',
-            }}
-          /> */}
-          <svg width="1002" height="1027" viewBox="0 0 1002 1027" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg viewBox="0 0 1025 1030" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
               ref={imageRef}
               fillRule="evenodd"
               clipRule="evenodd"
-              d="M1002 0H0V1027H1002V0ZM248.117 419.164C277.027 386.837 304.935 355.63 322.277 329.92C329.83 318.723 337.238 304.418 345.558 288.352C376.786 228.049 420.857 142.948 533.611 104.364C676.404 55.5009 809.406 128.359 843.676 172.424C845.407 174.649 847.182 176.903 848.988 179.196C882.936 222.304 927.93 279.436 901.201 429.392C873.051 587.325 802.062 702.066 707.819 794.121C613.575 886.176 445.079 917.588 337.372 908.426C229.666 899.264 -33.8898 794.557 163.573 521.447C185.086 489.645 217.175 453.763 248.117 419.164Z"
+              d="M1025 0H0V1030H1025V0ZM209.23 372.012C243.762 338.222 277.096 305.603 297.81 278.73C306.832 267.027 315.68 252.074 325.618 235.282C362.918 172.251 415.558 83.2998 550.237 42.9702C720.796 -8.10357 879.659 68.0511 920.593 114.109C922.661 116.435 924.78 118.79 926.938 121.187C967.487 166.245 1021.23 225.962 989.304 382.702C955.679 547.78 870.888 667.712 758.319 763.931C645.75 860.15 444.491 892.984 315.841 883.407C187.191 873.831 -127.612 764.387 108.247 478.921C133.943 445.681 172.272 408.175 209.23 372.012Z"
               fill="#16223F"
             />
           </svg>
