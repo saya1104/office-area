4c24525cf53a368708b9a576209f99bf2c579b30
kaneko1117 <yuta_kaneko@pj100.biz>
2023-05-26 10:49:56 +0900

    ✨build対応

--- pages/InterviewPage/OtaYuji.tsx
+++ pages/InterviewPage/OtaYuji.tsx
@@ -1,5 +1,6 @@
 import Footer from '../../components/layouts/Footer';
 import Header from '../../components/layouts/Header';
+
 import MemberInterviewCarousel from '../../components/templates/InterviewPage/InterviewTop/MemberInterviewCarousel';
 import InterviewDetail from '../../components/templates/InterviewPage/OtaYuji/InterviewDetail';
 import Top from '../../components/templates/InterviewPage/OtaYuji/Top';
